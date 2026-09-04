c9cca49af107386d2dce5776ec2244a55fca1d0f
elleined <demadegu@gmail.com>
2026-09-04 16:24:17 +0800

    Add model binding

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
@@ -20,5 +21,9 @@ class Comment extends Model
 
     public function author(): BelongsTo {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function reactions(): BelongsToMany {
+        return $this->belongsToMany(CommentReaction::class, 'comment_id');
     }
 }
--- app/Models/PostReaction.php
+++ app/Models/PostReaction.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class PostReaction extends Model
 {
     /** @use HasFactory<\Database\Factories\PostReactionFactory> */
     use HasFactory, HasUuids;
+
+
+    public function reactions(): BelongsToMany {
+        return $this->belongsToMany(Post::class, 'comment_id');
+    }
 }
